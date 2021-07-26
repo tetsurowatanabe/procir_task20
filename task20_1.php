2a9d9a026ae6f53a4bdd7175967617297bcca176
Tetsuro Watanabe <tetsurowatanabe0930@gmail.com>
2021-07-27 08:50:01 +0900

    HTML構文の記入

---
+++ task20_1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>課題20 ファイル1</title>
+</head>
+<body>
+</body>
+</html>
---
+++ task20_2.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>課題20 ファイル2</title>
+</head>
+<body>
+</body>
+</html>
